e5cceb12a80e0222cb4da243960d385818f474f2
LucassTeixeiraN <lgtn896@gmail.com>
2026-09-16 10:08:19 -0300

    refactor(mocks): move todos os mocks para uma pasta própria

---
+++ frontend/src/mocks/inventory.js
@@ -0,0 +1,106 @@
+export const mockInventoryCategories = [
+    {
+        id: 1,
+        name: "ALIMENTAÇÃO"
+    },
+    {
+        id: 2,
+        name: "VESTUÁRIO"
+    },
+    {
+        id: 3,
+        name: "HIGIENE"
+    },
+    {
+        id: 4,
+        name: "LIMPEZA"
+    },
+    {
+        id: 5,
+        name: "MEDICAMENTOS"
+    }
+];
+
+export const mockInventory = [
+    {
+        id: 1,
+        name: "Ração para cães",
+        category: "ALIMENTAÇÃO",
+        quantity: 80,
+        unit: "kg",
+        minimumQuantity: 20,
+        status: "NORMAL"
+    },
+    {
+        id: 2,
+        name: "Ração para gatos",
+        category: "ALIMENTAÇÃO",
+        quantity: 12,
+        unit: "kg",
+        minimumQuantity: 20,
+        status: "ESTOQUE_BAIXO"
+    },
+    {
+        id: 3,
+        name: "Cobertores",
+        category: "VESTUÁRIO",
+        quantity: 35,
+        unit: "un.",
+        minimumQuantity: 10,
+        status: "NORMAL"
+    },
+    {
+        id: 4,
+        name: "Fraldas infantis",
+        category: "HIGIENE",
+        quantity: 0,
+        unit: "un.",
+        minimumQuantity: 30,
+        status: "SEM_ESTOQUE"
+    },
+    {
+        id: 5,
+        name: "Sabonetes",
+        category: "HIGIENE",
+        quantity: 45,
+        unit: "un.",
+        minimumQuantity: 20,
+        status: "NORMAL"
+    },
+    {
+        id: 6,
+        name: "Arroz",
+        category: "ALIMENTAÇÃO",
+        quantity: 25,
+        unit: "kg",
+        minimumQuantity: 30,
+        status: "ESTOQUE_BAIXO"
+    },
+    {
+        id: 7,
+        name: "Feijão",
+        category: "ALIMENTAÇÃO",
+        quantity: 50,
+        unit: "kg",
+        minimumQuantity: 20,
+        status: "NORMAL"
+    },
+    {
+        id: 8,
+        name: "Produtos de limpeza",
+        category: "LIMPEZA",
+        quantity: 8,
+        unit: "un.",
+        minimumQuantity: 10,
+        status: "ESTOQUE_BAIXO"
+    },
+    {
+        id: 9,
+        name: "Antisséptico",
+        category: "MEDICAMENTOS",
+        quantity: 25,
+        unit: "un.",
+        minimumQuantity: 10,
+        status: "NORMAL"
+    }
+];
